8f3639db1fef029ebac48e195b808476a3592677
joren <babamyloves2002@gmail.com>
2023-02-05 18:25:50 -0800

    added no.3 functions :D

--- App.java
+++ App.java
@@ -11,21 +11,52 @@ public class App {
         int size = sc.nextInt();
         double[] arr = new double[size];
 
-
         for(int i = 0; i < size; i++) {
             arr[i] = sc.nextDouble();
         }
         Arrays.sort(arr);
 
-        System.out.println(getRange(arr[arr.length-1], arr[0]));
+        System.out.println("1. " + getRange(arr[arr.length-1], arr[0]));
         double range = getRange(arr[arr.length-1], arr[0]);
-        System.out.println(divideItByClass(classs, range));
+        System.out.println("2. " + divideItByClass(classs, range));
         double addToEachNum = divideItByClass(classs, range);
+
+        double[] numOfClasses = addNumToLowestNum(arr[0], addToEachNum, classs);
+
+        System.out.println("3. " + Arrays.toString(numOfClasses));
+
 
         sc.close();
 
     }
 
+    public static double[] addNumToLowestNum(double lowestNumFromArr, double numberToAdd, double classs) {
+
+        double[] output = new double[(int) classs];
+        double tempNum = lowestNumFromArr;
+        
+        for(int i = 0; i < classs; i++) {
+
+            output[i] = tempNum + numberToAdd;
+            tempNum = output[i];
+
+
+        }
+
+        return output;
+
+    }
+
+    // public static double[] addNums(double lowestNumFromArr, double numberToAdd, int classs) {
+
+        
+    //     for(int i = 0; i < classs; i++) {
+
+    //         tempNum = 
+
+    //     }
+
+    // }
 
     public static double getRange(double highestNum, double lowestNum) {
         double range = highestNum - lowestNum;
